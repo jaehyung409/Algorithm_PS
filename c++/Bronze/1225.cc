e685498a4a6c35b4c7b0bd3a0746086cd019e1b2
2021032188 <2021032188@hanyang.ac.kr>
2021-12-11 23:32:07 +0900

    strange multiplication

---
+++ c++/Bronze/1225.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+	char A[10001];
+	char B[10001];
+
+	cin >> A;
+	cin	>> B;
+
+	long long int sum{0};
+	
+	for(int i = 0; char a = A[i]; ++i){
+		for(int j = 0; char b = B[j]; ++j)
+			sum += (a - '0') * (b - '0');
+	}
+	
+	cout << sum << '\n';
+
+	return 0;
+
+}
