1e2acaf0ab52816a6ad02f7bdfc810d10d73a3a8
jaehyung409 <sonhs666@naver.com>
2025-01-11 22:00:12 +0900

    1781 Cup instant noodle

---
+++ c++/Gold/1781.cc
@@ -0,0 +1,60 @@
+//
+// Created by jaehy on 25. 1. 11.
+//
+#include <iostream>
+#include <queue>
+#include <stack>
+
+typedef std::pair<int, long long> il;
+
+struct Compare {
+    bool operator()(const il &a, const il &b) {
+        if (a.first == b.first) {
+            return a.second < b.second;
+        }
+        return a.first > b.first;
+    }
+};
+
+int main(){
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    std::priority_queue<il, std::vector<il>, Compare> heap;
+    int N, dead_line, time = 1;
+    long long reward;
+    std::cin >> N;
+    for (int i = 0; i < N; i++){
+        std::cin >> dead_line >> reward;
+        heap.emplace(dead_line, reward);
+    }
+    std::priority_queue<long long, std::vector<long long>, std::greater<>> ans;
+    std::queue<long long> temp;
+    long long sum = 0;
+    while(!heap.empty()){
+        while (ans.size() <= time) {
+            dead_line = heap.top().first;
+            if (dead_line > time) break;
+            reward = heap.top().second;
+            if (ans.size() + temp.size() < time) temp.push(reward);
+            else if (!ans.empty() && ans.top() < reward){
+                temp.push(reward);
+                ans.pop();
+            }
+            heap.pop();
+            if (heap.empty()) break;
+        }
+        while (!temp.empty()){
+            ans.push(temp.front());
+            temp.pop();
+        }
+        time++;
+    }
+    while (!ans.empty()){
+        sum += ans.top();
+        ans.pop();
+    }
+    std::cout << sum << '\n';
+
+    return 0;
+}
